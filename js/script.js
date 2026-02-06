8fd658aca745704eb332a6cab7335fab619115dd
Hilmy Abid Syafi Abiyyu <hilmyabidhasa@gmail.com>
2026-02-06 14:26:51 +0700

    add delete all button function and render the to do list

--- js/script.js
+++ js/script.js
@@ -15,19 +15,36 @@ function addTodo() {
             date: todoDate.value
         };
         todos.push(newTodo);
-        console.log("Todo added:", todos);
 
         todoInput.value = '';
         todoDate.value = '';
+
+        console.log(todos);
+        renderTodos();
     }
 }
 
+// Function to render todo items to the DOM
 function renderTodos() {
-    
+ const todoList = document.getElementById('todo-list');
+ 
+//  Clear existing list
+ todoList.innerHTML = '';
+
+//  Render each todo item
+    todos.forEach((item, _) => {
+        todoList.innerHTML += `
+        <li>
+            <p class="text-2xl">${item.todo} <span class="text-sm text-gray-500">(${item.date})</span></p>
+            <hr />
+        </li>`;
+    });
 }
 
+// Function to delete all todo items
 function deleteAllTodos() {
-
+    todos = [];
+    renderTodos();
 }
 
 function filterTodos() {
